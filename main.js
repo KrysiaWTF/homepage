bc9dc04545de013b7e093053924315659045022d
Krysia Janik <a.krystyna.janik@gmail.com>
2020-01-28 16:11:10 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const name = 'Krysia';
+const age = '26';
+
+console.log ('Lew nie sprzymierza się z kojotem');
+console.log (`Cześć, mam na imię ${name}, mam ${age} lat i od niedawna uczę się programować`);
