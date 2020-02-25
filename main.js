026d971bef84229bbffb3c9375fd3eff65a42b55
Krysia Janik <a.krystyna.janik@gmail.com>
2020-02-25 10:15:00 +0100

    JS - added function and CSS - styles changed

--- main.js
+++ main.js
@@ -3,3 +3,28 @@ const age = '26';
 
 console.log ('Lew nie sprzymierza się z kojotem');
 console.log (`Cześć, mam na imię ${name}, mam ${age} lat i od niedawna uczę się programować`);
+
+
+const about = document.querySelector('.about__paragraph--js');
+
+about.innerHTML = `Mam na imię ${name}, mam ${age} lat i od niedawna uczę się programować.`
+
+
+
+
+const myAge = 26
+
+function ageCalculate (x) {
+  x = x - myAge
+  return x
+}
+
+let ageDifference = ageCalculate (22)
+
+if (ageDifference > 0) {
+  console.log(`Jesteś o tyle lat ode mnie starszy: ${ageDifference}`)
+} else if (ageDifference < 0){
+  console.log(`Jesteś o tyle lat ode mnie młodszy: ${Math.abs(ageDifference)}`)
+} else {
+  console.log(`Jesteś w moim wieku!`)
+}
