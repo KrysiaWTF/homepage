eb4fd92b05c5d2ee2075b50ef5e33b66278ce7eb
Krysia Janik <a.krystyna.janik@gmail.com>
2020-03-04 10:44:25 +0100

    feat interactive buttons and style refresh

--- main.js
+++ main.js
@@ -4,14 +4,6 @@ const age = '26';
 console.log ('Lew nie sprzymierza się z kojotem');
 console.log (`Cześć, mam na imię ${name}, mam ${age} lat i od niedawna uczę się programować`);
 
-
-const about = document.querySelector('.about__paragraph--js');
-
-about.innerHTML = `Mam na imię ${name}, mam ${age} lat i od niedawna uczę się programować.`
-
-
-
-
 const myAge = 26
 
 function ageCalculate (x) {
@@ -28,3 +20,17 @@ if (ageDifference > 0) {
 } else {
   console.log(`Jesteś w moim wieku!`)
 }
+
+
+
+const menuDrop = document.querySelector('.navigation__list--js')
+const navigationDrop = document.querySelector('.navigation__drop--js')
+
+
+menuDrop.addEventListener('mouseenter', () => {
+  navigationDrop.classList.add('navigation__drop--visible')
+});
+
+menuDrop.addEventListener('mouseleave', () => {
+  navigationDrop.classList.remove('navigation__drop--visible')
+});
